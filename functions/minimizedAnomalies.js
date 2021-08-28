0f9dcebbe12773e6fd53a3a5a01ed3c5a9f7ee0a
Teodors <teodors@imsystems.lv>
2021-08-28 12:36:27 +0200

    Local cors allowance in function
    Local cors allowance in function

--- functions/minimizedAnomalies.js
+++ functions/minimizedAnomalies.js
@@ -1,8 +1,12 @@
 const { smallBatch } = require("../data/anomalies");
 
 exports.handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(smallBatch)
-    }
-}
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(smallBatch),
+  };
+};
